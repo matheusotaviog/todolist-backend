85d68ab6755fafc892b8dea76c69cbd430ee4a2a
Matheus Otavio Gonçalves <matheusotaviog@outlook.com>
2023-10-12 14:24:35 -0300

    Modelado tabela da entidade UserModel

--- src/main/java/br/com/matheusotaviog/todolist/user/UserModel.java
+++ src/main/java/br/com/matheusotaviog/todolist/user/UserModel.java
@@ -1,12 +1,27 @@
 package br.com.matheusotaviog.todolist.user;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tab_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
     
+    @CreationTimestamp
+    private LocalDate createdAt;
 }
