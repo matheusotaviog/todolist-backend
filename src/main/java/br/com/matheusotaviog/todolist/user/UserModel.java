e48913796c5ae744c9db94ceb0367fb81f9fda48
Matheus Otavio Gonçalves <matheusotaviog@outlook.com>
2023-10-12 13:57:41 -0300

    Adicionado biblioteca lombok ao UserModel

--- src/main/java/br/com/matheusotaviog/todolist/user/UserModel.java
+++ src/main/java/br/com/matheusotaviog/todolist/user/UserModel.java
@@ -1,32 +1,12 @@
 package br.com.matheusotaviog.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username;
     private String name;
     private String password;
     
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
