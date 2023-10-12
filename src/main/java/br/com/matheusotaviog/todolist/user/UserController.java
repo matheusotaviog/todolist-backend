f2cd5e6893778a35893ac4807e3c87f6b500607d
Matheus Otavio Gonçalves <matheusotaviog@outlook.com>
2023-10-12 16:06:03 -0300

    Criptografando senha do usuário

--- src/main/java/br/com/matheusotaviog/todolist/user/UserController.java
+++ src/main/java/br/com/matheusotaviog/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,9 @@ public class UserController {
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O username " + userModel.getUsername() + " já existe.");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
